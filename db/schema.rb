5f0a5a48bc017f06772c0f18e5b22e4753b887da
hasini <hasini.psk@gmail.com>
2011-11-07 16:05:01 +0000

    minor

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111106174107) do
+ActiveRecord::Schema.define(:version => 20111106174956) do
+
+  create_table "appointments", :force => true do |t|
+    t.integer  "doctor_id"
+    t.integer  "patient_id"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.string   "hosiptal_name"
+    t.string   "purpose"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "uploads", :force => true do |t|
+    t.string   "my_file"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
     t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
@@ -40,10 +58,5 @@ ActiveRecord::Schema.define(:version => 20111106174107) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-  create_table "uploads", :force => true do |t|
-    t.string   "my_file"
-    t.datetime "created_at"
-    t.datetime "updated_at"
- end
 
 end
