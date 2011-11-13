4af8fbed70c34493ff5b64582b4f9a390c246a2c
Eshwar Goutama <eshwardeep@gmail.com>
2011-11-13 13:38:58 +0530

    added has many relation for patient and images

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,8 +1,7 @@
 class Patient < User
-  has_many :appointments
-  
+
+  has_many :appointments  
   has_many :uploads  
- # has_many :patient_images
-  has_one :patient_image
+  has_many :patient_images
 
 end
