3f642de1996af572b7d05f8f234d36730c3539fc
hasini <hasini.psk@gmail.com>
2011-11-15 01:07:38 +0530

    minor

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+
+ Admin.create(:email => "admin@example.com", :password => "password",  "confirm_password" => "password")
