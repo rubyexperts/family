3b81cf3cd5b15ac96f39c789c53044dddf129265
sumudhar <sumudhar@gmail.com>
2011-11-16 02:16:43 +0530

    Changed the Admin view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,56 @@
 class UsersController < ApplicationController
-
    before_filter :authenticate_user!
    before_filter :selected_tab
    layout 'default'   
    
    def index
+     @users=User.all
+      respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml =>@users }
+    end
    end
    
    def new
      @user = User.new
    end
-   
+
+   def show
+     @user = User.find(params[:id])
+     respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @user }
+     end
+   end
+
+   def edit
+     @user= User.find(params[:id])
+   end
+
+   def update
+    @user = User.find(params[:id])
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to(@user,
+                      :notice => 'User Was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @user.errors,
+                      :status => :unprocessable_entity }
+      end
+    end
+   end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+   respond_to do |format|
+      format.html { redirect_to(users_url) }
+      format.xml  { head :ok }
+    end
+  end
+
    def create_master_account
      @user = User.new(params[:user])
      if @user.valid?
