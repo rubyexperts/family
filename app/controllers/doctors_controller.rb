96190267a85f2342661c626c8c6cec72c662999a
Eshwar Goutama <eshwardeep@gmail.com>
2011-11-15 10:46:48 +0530

    removed unncessary code

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -5,20 +5,10 @@ class DoctorsController < ApplicationController
 
     def index
 	  @my_approved_appointments = Appointment.where('doctor_id = ? and approve_status = ?', current_user.id, 2)
-	  
-	  puts "-------------------------"
-	  puts @my_approved_appointments.inspect
-	  puts "-------------------------"
-	  
 	  @my_patients = []
 	  @my_approved_appointments.each do |appointment|
 	     @my_patients << Patient.find_by_id(appointment.patient_id)
 	  end
-	  
-	  puts "-------------------------"
-	  puts @my_patients.inspect
-	  puts "-------------------------"
-	  
 	end
 	
 	
