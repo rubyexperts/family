2fdca92717b4563a9ba9f79f1e9fb823f5e7f937
Eshwar Goutama <eshwardeep@gmail.com>
2011-11-29 22:45:16 +0530

    added subdomaining in signup part

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -5,30 +5,44 @@ class Devise::RegistrationsController < ApplicationController
 
   # GET /resource/sign_up
   def new
-
-
     @select = "sign_up"
     resource = build_resource({})
+    @site = Site.new   # written by Eshwar
     respond_with_navigational(resource){ render_with_scope :new }
   end
 
   # POST /resource
+ # def create
+  #  build_resource
+ #   raise "Eshwar".inspect
+ #   if resource.save
+ #     if resource.active_for_authentication?
+ #       set_flash_message :notice, :signed_up if is_navigational_format?
+ #       sign_in(resource_name, resource)
+ #       respond_with resource, :location => redirect_location(resource_name, resource)
+ #     else
+ #       set_flash_message :notice, :inactive_signed_up, :reason => inactive_reason(resource) if is_navigational_format?
+  #      expire_session_data_after_sign_in!
+  #      respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+  #    end
+  #  else
+  #    clean_up_passwords(resource)
+  #    respond_with_navigational(resource) { render_with_scope :new }
+  #  end
+  # end
+  
   def create
-    build_resource
-
-    if resource.save
-      if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_navigational_format?
-        sign_in(resource_name, resource)
-        respond_with resource, :location => redirect_location(resource_name, resource)
-      else
-        set_flash_message :notice, :inactive_signed_up, :reason => inactive_reason(resource) if is_navigational_format?
-        expire_session_data_after_sign_in!
-        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
-      end
+    @user = User.new(params[:user])
+    @site = Site.new(params[:site])
+    if @user.valid? && @site.valid?
+       @site.save
+       @user.type = "Doctor"
+       @user.site = @site
+       @user.save
+       flash[:notice] = "You have signed up successfully. If enabled, a confirmation was sent to your e-mail."
+       redirect_to("http://#{@site.name}.familypractice.in:3000")
     else
-      clean_up_passwords(resource)
-      respond_with_navigational(resource) { render_with_scope :new }
+      render :action => :new
     end
   end
 
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,22 +2,22 @@ class RegistrationsController < Devise::RegistrationsController
   prepend_view_path "app/views/devise"
   layout 'login'
 	 
-    def new
-	   super
-	 end
+   # def new
+#	   super
+#	 end
 	 
-	 def create
-	   @user = User.new(params[:user])
-       if @user.save
-	      flash[:notice] = "You have signed up successfully. If enabled, a confirmation was sent to your e-mail."
-	      redirect_to "/home"
-	   else
-	      render :action => :new
-	   end
-	 end
+#	 def create
+	   #@user = User.new(params[:user])
+      # if @user.save
+	   #   flash[:notice] = "You have signed up successfully. If enabled, a confirmation was sent to your e-mail."
+	    #  redirect_to "/home"
+	  # else
+	#      render :action => :new
+	#   end
+	# end
 	 
-	 def update
-	   super
-	 end
+	# def update
+	 #  super
+	# end
 	 
 end
